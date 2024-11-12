4059623e07c7af1f372f871e4c151183de637186
ZZalan001 <zima.zalan1@gmail.com>
2024-11-12 14:29:23 +0100

    teljes feladat

--- Sakk/Allas.cs
+++ Sakk/Allas.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Sakk
 {
-    public class Allas
+    public class Allas : IComparable<Allas>
     {
         DateTime mentesIdo;
         /// <summary>
@@ -18,7 +18,7 @@ namespace Sakk
         /// <param name="tabla"> 8x8 as string tömb, benne a bábuk pl vH --> világos huszár </param>
         /// <param name="vilagosJon"> true, ha a világos játékos jön </param>
         /// <param name="mentesIdo"> egy dátum-idő szövegesen, ha 0 vagy kihagyjuk akkor az aktuális időpontot menti</param>
-        public Allas(int id, string vilagos, string sotet, string[,] tabla, bool vilagosJon, string mentesIdo = "0")
+        public Allas(int id, string vilagos, string sotet, string[,] tabla, bool vilagosJon, string mentesIdo = "1900.01.01")
         {
             Id = id;
             Vilagos = vilagos;
@@ -27,7 +27,26 @@ namespace Sakk
             VilagosJon = vilagosJon;
             MentesIdo = DateTime.Parse(mentesIdo);
         }
-
+        public Allas(string sor) 
+        {
+            string[] s = sor.Split(';');     
+            Id = int.Parse(s[0]);
+            Vilagos = s[1];
+            Sotet = s[2];
+            VilagosJon = bool.Parse(s[3]);
+            MentesIdo = DateTime.Parse(s[4]);
+            string[,] ujTabla = new string[8, 8];
+            int index = 5;
+            for (int i = 0; i < 8; i++)
+            {
+                for (global::System.Int32 j = 0; j < 8; j++)
+                {
+                    ujTabla[i, j] = s[index];
+                    index++;
+                }
+            }
+            Tabla = ujTabla;
+        }
         public int Id {  get; set; }
         public string Vilagos { get; set; }
         public string Sotet { get; set; }
@@ -36,7 +55,7 @@ namespace Sakk
         public DateTime MentesIdo {
             get { return mentesIdo; }
             set {
-                if (value == DateTime.Parse("0"))
+                if (value == DateTime.Parse("1900.01.01"))
                 {
                     mentesIdo = DateTime.Now;
                 }
@@ -54,8 +73,38 @@ namespace Sakk
             sb.Append($"Világos játékos: {Vilagos}\n");
             sb.Append($"Sötét játékos: {Sotet}\n");
             string kovetkezo = VilagosJon ? "világos" : "sötét";
-            sb.Append($"Következő játékos: {kovetkezo}");
+            sb.Append($"Következő játékos: {kovetkezo}\n");
+            for (int i = 0; i < Tabla.GetLength(0); i++)
+            {
+                for (global::System.Int32 j = 0; j < Tabla.GetLength(1); j++)
+                {
+                    sb.Append(Tabla[i,j]+" | ");
+                }
+                sb.Append('\n');
+                sb.Append(new string('-', 40));
+                sb.Append('\n');
+            }
             return sb.ToString();
+        }
+        public string KovetkezoJatekos()
+        {
+            return VilagosJon ? Vilagos : Sotet;
+        }
+
+        public int CompareTo(Allas allas)
+        {
+            if(this.MentesIdo < allas.MentesIdo)
+            {
+                return -1;
+            }
+            else if(this.MentesIdo > allas.MentesIdo) 
+            {
+                return 1; 
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
--- Sakk/Program.cs
+++ Sakk/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Sakk
 {
     internal class Program
     {
+        static List<Allas> allasok = new List<Allas>();
         static void Main(string[] args)
         {
             string[,] tabla1 = new string[8, 8];
@@ -15,16 +17,113 @@ namespace Sakk
             {
                 for (global::System.Int32 j = 0; j < tabla1.GetLength(1); j++)
                 {
-                    tabla1[i, j] = "0";
+                    tabla1[i, j] = "  ";
                 }
                 
             }
             Allas aktualisAllas = new Allas(0, "Kiss Elemér", "Füty Imre", tabla1, true);
-            Console.WriteLine(aktualisAllas);
+            allasok.Add(aktualisAllas);
+
+            string[] bemenet = File.ReadAllLines("mentesek.txt");
+            foreach (string sor in bemenet)
+            {
+                allasok.Add(new Allas(sor));
+            }
+            foreach (Allas a in allasok)
+            {
+                Console.WriteLine(a.ToString());
+                Console.WriteLine();
+            }
+
+            //Kivel játszott Kiss Elemér? Az összes játékostárs nevét írassa ki egymás alá!
+            List<string> KEJatTars = new List<string>();
+            foreach (Allas a in allasok)
+            {
+                if(a.Vilagos == "Kiss Elemér")
+                    KEJatTars.Add(a.Sotet);
+                if(a.Sotet == "Kiss Elemér")
+                    KEJatTars.Add(a.Vilagos);
+            }
+
+            //Legrégebbi mentés
+            Allas legkorabbi = allasok[0];
+            for (int i = 0; i < allasok.Count; i++)
+            {
+                if (allasok[i].MentesIdo < legkorabbi.MentesIdo)
+                    legkorabbi = allasok[i];
+            }
+
+            //Játszott-e Kaszparov nevű játékos? Nem tudjuk, hogy ez-e a teljes neve!
+
+            //Tanár megoldás
+            int index = 0;
+            while (index < allasok.Count && !(allasok[index].Vilagos.Contains("Kaszparov") || allasok[index].Sotet.Contains("Kaszparov")))
+            {
+                index++;
+            }
+            if (index < allasok.Count)
+            {
+                Console.WriteLine("Van Kaszparov a listában.");
+            }
+            else
+            {
+                Console.WriteLine("Nincs Kaszparov a listában.");
+            }
+
+            // Saját megoldás
+            //bool vanbenne = false;
+            //foreach (Allas a in allasok)
+            //{
+            //    if (a.Sotet.Contains("Kaszparov") || a.Vilagos.Contains("Kaszparov"))
+            //    {
+            //        vanbenne = true;
+            //    }
+            //}
+
+            //Írassuk ki azt a mentést, amiben van világos bástya (vB) a táblán
+            index = 0;
+            while (index < allasok.Count && !(TartalmazErtek(allasok[index].Tabla,"vB")))
+            {
+                index++;
+            }
+            if (index < allasok.Count)
+            {
+                Console.WriteLine(allasok[index]);
+            }
+            else
+            {
+                Console.WriteLine("Nincs ilyen állás.");
+            }
 
 
-
+            Console.WriteLine(string.Join("\n",KEJatTars));
+            Console.WriteLine($"Az első mentett állás következő játékosa: {allasok[0].KovetkezoJatekos()}");
+            Console.WriteLine($"A legkorábbi mentést játszotta: {legkorabbi.Vilagos} és {legkorabbi.Sotet}");
+            //Console.WriteLine($"Kaszparov nevű játékos játszott: {vanbenne}");
+            Console.WriteLine(allasok[0].CompareTo(allasok[1]));
             Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Az adott kétdimenziós stringtömb (mátrix) tartalmazza-e az adott stringet elemként?
+        /// </summary>
+        /// <param name="matrix"> A kétdimenziós stringtömb </param>
+        /// <param name="ertek"> A keresett elem </param>
+        /// <returns> True, ha tartalmazza, false egyébként </returns>
+        static bool TartalmazErtek(string[,] matrix, string ertek)
+        {
+            bool van = false;
+            for(int i=0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if(matrix[i,j] == ertek) 
+                    { 
+                        van = true; 
+                    }
+                }
+            }
+            return van;
         }
     }
 }
